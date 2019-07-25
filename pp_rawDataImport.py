d492ed8da8452b1e56c92a8f17fd68454cc1471e
xiashiwendao <xiashiwendao@sina.com>
2019-07-25 22:53:05 +0800

    some new commit

--- pp_rawDataImport.py
+++ pp_rawDataImport.py
@@ -1,12 +1,14 @@
-# -*- coding:utf-8 -*-
-import numpy as np
-import pandas as pd
-from sklearn.externals import joblib
-import pymysql
-import argparse
-from time import gmtime, strftime
-import warnings
+# 将原始csv文件中数据导入到mysql中，便于快速查询
 import os
+import pandas as pd
+import numpy as np
+import pymysql
+import traceback
+
+base_path = "C:\\Users\\wenyang.zhang\\Documents\\MySpace\\Workspace\\PredictionPlatform\\Datasource"
+filePaht = os.path.join(base_path, "BANNER.csv")
+_chunksize = 100000
+reader = pd.read_csv(filePaht, iterator=True, chunksize=_chunksize)
 
 # Mysql info
 user = 'root'
@@ -15,60 +17,70 @@ host = '127.0.0.1'
 write_db = 'pp'
 write_table = 'banner'
 
-# Save the prediction result to mysql
-def save_result(df, which_date):
-    conn = pymysql.connect(user=user, password=password, database=write_db, host=host, charset='utf8')
+def writeToDB(data):
+    with conn.cursor() as cur:
+        print("数据数量: ", len(data))
+        # loop for the DataFrame
+        for i in range(len(data)):
+            #print("开始计数: ", i)
+            writing_sql = """insert into `banner_full` 
+            (`REPORT_DATE`
+            ,`STORE_BKEY`
+            ,`BANNER_NAME`
+            ,`STORE_TYPE`
+            ,`PRODUCT_BKEY`
+            ,`BRAND_NAME`
+            ,`AMOUNT_PER_KG`
+            ,`PRICE_PER_KG`
+            ,`HAS_GROUND`
+            ,`WEEK_OF_YEAR`
+            ,`MONTH_OF_YEAR`
+            ,`YEAR_OF_WEEK`
+            ,`IS_VALENTINE`
+            ,`IS_TEACHER`
+            ,`IS_C_VALENTINE`
+            ,`IS_CHILDREN`
+            ,`IS_NEWYEAR`
+            ,`IS_CHRISTMAS`
+            ,`IS_12`
+            ,`IS_11`
+            ,`IS_618`
+            ,`IS_SUMMER`
+            ,`IS_WINTER`
+            ,`STORE_COUNT`
+            ,`CITY_COUNT`
+            ,`RN`
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            cur.execute(writing_sql, (str(data[i, 0]),  str(data[i, 1]),  str(data[i, 2]),
+                                        str(data[i, 3]), str(data[i, 4]),  str(data[i, 5]),
+                                        str(data[i, 6]), str(data[i, 7]),  str(data[i, 8]),  str(data[i, 9]),
+                                        str(data[i, 10]), str(data[i, 11]),  str(data[i, 12]),
+                                        str(data[i, 13]), str(data[i, 14]),  str(data[i, 15]),  str(data[i, 16]),
+                                        str(data[i, 17]), str(data[i, 18]),  str(data[i, 19]),
+                                        str(data[i, 20]), str(data[i, 21]),  str(data[i, 22]),  str(data[i, 23]),
+                                        str(data[i, 24]), str(data[i, 25])))
+            #cur.execute(writing_sql)
+    # commit to mysql
+    conn.commit()
 
-    # convert dataframe to array
-    data = np.array(df)
-    try:
-        with conn.cursor() as cur:
-            # loop for the DataFrame
-            for i in range(len(data)):
-                writing_sql = """insert into `carre_prediction` 
-                (`forcast_day_1`, `forcast_day_2`, `forcast_day_3`, `forcast_day_4`, `forcast_day_5`, 
-                `forcast_day_6`, `forcast_day_7`, `partition_date`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
-                cur.execute(writing_sql, (str(data[i, 0]),  str(data[i, 1]),  str(data[i, 2]),
-                                          str(data[i, 3]), str(data[i, 4]),  str(data[i, 5]),
-                                          str(data[i, 6]), str(data[i, 7])))
-        # commit to mysql
-        conn.commit()
-
-    except:
-        print('There is exception happened!')
-        conn.rollback()
-    finally:
+conn = pymysql.connect(user="root", password="root", database=write_db, host="127.0.0.1", charset='utf8')
+try:
+    counter = 0
+    for chunk in reader:
+        # convert dataframe to array
+        data = chunk.values
+        db_data = []
+        for item in data:
+            #db_data.append(item[0].split("\t"))
+            db_data.append(item)
+        # writeToDB(db_data)
+        db_data = np.array(db_data)
+        writeToDB(db_data)
+        print("第" ,counter ,"轮已经完成")
+        counter += 1
+except Exception:
+    print('There is exception happened!', Exception)
+    traceback.print_exc()
+    conn.rollback()
+finally:
         conn.close()
-
-
-# Start to make prediction
-if __name__ == '__main__':
-    import time
-    st = time.time()
-
-    base_path = "C:\\Users\\wenyang.zhang\\Documents\\MySpace\\Workspace\\AIPlatform\\Datasource"
-    filePaht = os.path.join(base_path, "BANNER.csv")
-    chunksize = 1000000
-    reader = pd.read_csv(filePaht, iterator=True, chunksize=5)
-    reader.get_chunk
-    df = get_mysql_data(which_day, has_label=True, is_local=False)
-    check_data(df)
-    model = get_model()
-
-
-    pred = model.predict(df)
-
-    # Here is the prediction columns
-    pred_cols = []
-    for i in range(7):
-        pred_cols.append('forcast_day_'+str(i + 1))
-
-    pred_df = pd.DataFrame(pred)
-    pred_df.columns = pred_cols
-
-    # save prediction to mysql
-    save_result(pred_df, which_day)
-
-    et = time.time()
-    print('All steps takes %.2f seconds'%(et-st))
-    print('All finished!')
--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import pymysql
 import traceback
 
 base_path = "C:\\Users\\wenyang.zhang\\Documents\\MySpace\\Workspace\\PredictionPlatform\\Datasource"
-filePaht = os.path.join(base_path, "BANNER_00.csv")
-_chunksize = 10
+filePaht = os.path.join(base_path, "BANNER.csv")
+_chunksize = 5000
 reader = pd.read_csv(filePaht, iterator=True, chunksize=_chunksize)
 
 # Mysql info
@@ -17,69 +17,67 @@ write_db = 'pp'
 write_table = 'banner'
 
 def writeToDB(data):
-    try:
-        with conn.cursor() as cur:
-            print("数据数量: ", len(data))
-            # loop for the DataFrame
-            for i in range(len(data)):
-                print("开始计数: ", i)
-                writing_sql = """insert into `banner` 
-                (`REPORT_DATE`
-                ,`STORE_BKEY`
-                ,`BANNER_NAME`
-                ,`STORE_TYPE`
-                ,`PRODUCT_BKEY`
-                ,`BRAND_NAME`
-                ,`AMOUNT_PER_KG`
-                ,`PRICE_PER_KG`
-                ,`HAS_GROUND`
-                ,`WEEK_OF_YEAR`
-                ,`MONTH_OF_YEAR`
-                ,`YEAR_OF_WEEK`
-                ,`IS_VALENTINE`
-                ,`IS_TEACHER`
-                ,`IS_C_VALENTINE`
-                ,`IS_CHILDREN`
-                ,`IS_NEWYEAR`
-                ,`IS_CHRISTMAS`
-                ,`IS_12`
-                ,`IS_11`
-                ,`IS_618`
-                ,`IS_SUMMER`
-                ,`IS_WINTER`
-                ,`STORE_COUNT`
-                ,`CITY_COUNT`
-                ,`RN`
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-                cur.execute(writing_sql, (str(data[i, 0]),  str(data[i, 1]),  str(data[i, 2]),
-                                          str(data[i, 3]), str(data[i, 4]),  str(data[i, 5]),
-                                          str(data[i, 6]), str(data[i, 7]),  str(data[i, 8]),  str(data[i, 9]),
-                                          str(data[i, 10]), str(data[i, 11]),  str(data[i, 12]),
-                                          str(data[i, 13]), str(data[i, 14]),  str(data[i, 15]),  str(data[i, 16]),
-                                          str(data[i, 17]), str(data[i, 18]),  str(data[i, 19]),
-                                          str(data[i, 20]), str(data[i, 21]),  str(data[i, 22]),  str(data[i, 23]),
-                                          str(data[i, 24]), str(data[i, 25])))
-                #cur.execute(writing_sql)
-                print("++++++++++++++++++ 成功执行！++++++++++++++++++")
-        # commit to mysql
-        conn.commit()
-
-    except Exception:
-        print('There is exception happened!', Exception)
-        traceback.print_exc()
-        conn.rollback()
-    finally:
-        conn.close()
+    with conn.cursor() as cur:
+        print("数据数量: ", len(data))
+        # loop for the DataFrame
+        for i in range(len(data)):
+            print("开始计数: ", i)
+            writing_sql = """insert into `banner` 
+            (`REPORT_DATE`
+            ,`STORE_BKEY`
+            ,`BANNER_NAME`
+            ,`STORE_TYPE`
+            ,`PRODUCT_BKEY`
+            ,`BRAND_NAME`
+            ,`AMOUNT_PER_KG`
+            ,`PRICE_PER_KG`
+            ,`HAS_GROUND`
+            ,`WEEK_OF_YEAR`
+            ,`MONTH_OF_YEAR`
+            ,`YEAR_OF_WEEK`
+            ,`IS_VALENTINE`
+            ,`IS_TEACHER`
+            ,`IS_C_VALENTINE`
+            ,`IS_CHILDREN`
+            ,`IS_NEWYEAR`
+            ,`IS_CHRISTMAS`
+            ,`IS_12`
+            ,`IS_11`
+            ,`IS_618`
+            ,`IS_SUMMER`
+            ,`IS_WINTER`
+            ,`STORE_COUNT`
+            ,`CITY_COUNT`
+            ,`RN`
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            cur.execute(writing_sql, (str(data[i, 0]),  str(data[i, 1]),  str(data[i, 2]),
+                                        str(data[i, 3]), str(data[i, 4]),  str(data[i, 5]),
+                                        str(data[i, 6]), str(data[i, 7]),  str(data[i, 8]),  str(data[i, 9]),
+                                        str(data[i, 10]), str(data[i, 11]),  str(data[i, 12]),
+                                        str(data[i, 13]), str(data[i, 14]),  str(data[i, 15]),  str(data[i, 16]),
+                                        str(data[i, 17]), str(data[i, 18]),  str(data[i, 19]),
+                                        str(data[i, 20]), str(data[i, 21]),  str(data[i, 22]),  str(data[i, 23]),
+                                        str(data[i, 24]), str(data[i, 25])))
+            #cur.execute(writing_sql)
+    # commit to mysql
+    conn.commit()
 
 conn = pymysql.connect(user="root", password="root", database=write_db, host="127.0.0.1", charset='utf8')
-for chunk in reader:
-    # convert dataframe to array
-    data = chunk.values
-    db_data = []
-    for item in data:
-        db_data.append(item[0].split("\t"))
-    # writeToDB(db_data)
-    db_data = np.array(db_data)
-    writeToDB(db_data)
-
+try:
+    for chunk in reader:
+        # convert dataframe to array
+        data = chunk.values
+        db_data = []
+        for item in data:
+            #db_data.append(item[0].split("\t"))
+            db_data.append(item)
+        # writeToDB(db_data)
+        db_data = np.array(db_data)
+        writeToDB(db_data)
+except Exception:
+    print('There is exception happened!', Exception)
+    traceback.print_exc()
+    conn.rollback()
+finally:
+        conn.close()
 
