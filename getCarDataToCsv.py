c97bf245a3268ecf3ab33340f967f3bb9cb656bc
xiashiwendao <xiashiwendao@sina.com>
2019-07-30 21:29:29 +0800

    implement stacking regression

---
+++ getCarDataToCsv.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import pymysql
+import pandas as pd
+
+conn = pymysql.connect(user='root',password='root',database='pp',host='localhost',charset='utf8')
+query = "select * from banner_car"
+df = pd.read_sql(query,conn)
+# df.to_csv("dataset\\banner_car.csv")
+print(len(df))
