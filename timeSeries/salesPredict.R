4de0928df9172eeed1b7d8992b7a92a83286bd0f
xiashiwendao <xiashiwendao@sina.com>
2019-08-06 22:24:48 +0800

    add time serias implements

---
+++ timeSeries/salesPredict.R
@@ -0,0 +1,98 @@
+library(sqldf)
+# get trends data
+trend = read.csv("dataset\\SalesTrend.csv")
+trend = sqldf("select REPORT_DATE, sum(QLI) as QLI from trend where BANNER_NAME='Carrefour' group by REPORT_DATE")
+head(trend, 1)
+
+# get banner data
+banner_car = read.csv("dataset\\banner_carr.csv")
+banner_car = sqldf("select REPORT_DATE, avg(YEAR_OF_WEEK) as year ,avg(MONTH_OF_YEAR) as month from banner_car group by REPORT_DATE")
+head(banner_car)
+
+# Merge the data
+banner_trend = merge(banner_car, trend, by="REPORT_DATE")
+head(banner_trend)
+banner_trend_agg = sqldf("select year, month, sum(qli) as qli from banner_trend group by year, month")
+head(banner_trend_agg, 100)
+banner_trend_agg=banner_trend_agg[-1,]
+
+banner_trend_agg = data.frame( banner_trend_agg, row.names = "year", )
+head(banner_trend_agg, 100)
+# write to csv
+#write.csv(banner_trend, "dataset\\banner_trend.csv")
+PRICE <- structure(list(
+  DATE = c(20070103L, 20070104L, 20070105L, 20070108L, 20070109L,
+           20070110L, 20070111L, 20070112L, 20070115L),
+  CLOSE = c(54.7, 54.77, 55.12, 54.87, 54.86, 54.27, 54.77, 55.36, 55.76)),
+  .Names = c("DATE", "CLOSE"), class = "data.frame",
+  row.names = c("1", "2", "3", "4", "5", "6", "7", "8", "9"))
+PRICE
+library(xts)  # loads/attaches xts
+# Convert DATE to Date class
+PRICE$DATE <- as.Date(as.character(PRICE$DATE),format="%Y%m%d")
+# create xts object
+x <- xts(PRICE$CLOSE,PRICE$DATE)
+
+
+
+
+library(zoo)
+weekIli = data.frame(week=c(21,22,23,24,25,21,22,23), ili=c(11,14,34,56,56,67,4,45))
+ts(weekIli$ILI,start = 21,end = 25,frequency = 5)
+
+
+library(zoo)
+library(ggplot2)
+docData = data.frame(mon=c(1,2,1,2),yr=c(1991,1991,1992,1992),doc=c(1,2,2,4),date=c("1991/10/02","1991/11/01","1991/12/01","1992/01/01"))
+docData
+docData = data.frame(docData, )
+docData <- read.zoo(docData[c("date", "doc")], FUN = as.yearmon, format = "%YYYY%MM")
+as.vector(docData[c("date", "doc")])
+docData =xts(docData$doc, order.by = as.Date(as.character(docData$yr), format='%Y'))
+docData
+
+m1.co2=arima(docData, order=c(0,1,1), seasonal = list(order=c(0,1,1), period=12))
+
+
+
+
+
+v2=c(12,13,15,17,18,12,11,12) 
+v2
+v2.ts<-ts(v2, frequency=12, start=c(1996,7), end=c(1997,10)) 
+v2.ts
+
+banner_trend_agg["qli"]
+trend.ts=ts(banner_trend_agg[["qli"]], start = c(2016,9),end = c(2018,8), frequency = 12)
+trend.ts
+
+trend.ts=arima(trend.ts, order=c(0,1,1), seasonal = list(order=c(0,1,1), period=12))
+predict(trend.ts, 3)
+
+
+
+ts(lst, frequency = 12, start=c(2016,9),end=c(2018,8))
+lst
+matrix=data.matrix(banner_trend_agg["qli"])
+c(lst)
+banner_trend_agg[["qli"]]
+banner_trend_agg["qli"]
+
+library(randomForest)
+library(CrossR)
+train_test_split(banner_trend_agg)
+randomForest()
+
+cluster1 <- data.frame(a=1:5, b=11:15, c=21:25, d=31:35)
+cluster1
+colMeans(cluster1)
+
+data("iris")
+head(iris,1)
+
+data(airquality)
+set.seed(131)
+ozone.rf <- randomForest(Ozone ~ ., data=airquality, mtry=3,
+                         importance=TRUE, na.action=na.omit)
+print(ozone.rf)
+head(airquality,5)
---
+++ timeSeries/salesPredict_ver2.R
@@ -0,0 +1,80 @@
+library(randomForest)
+library(sqldf)
+library(CrossR)
+library(caret)
+df = read.csv("dataset\\SalesTrend.csv")
+head(df, 1)
+trend = sqldf("select REPORT_DATE, sum(QLI) as QLI
+,avg(LAST_1_MONTH_AVG) as LAST_1_MONTH_AVG
+,avg(LAST_1_MONTH_MIN) as LAST_1_MONTH_MIN
+,avg(LAST_1_MONTH_MAX) as LAST_1_MONTH_MAX
+,avg(LAST_1_MONTH_SUM) as LAST_1_MONTH_SUM
+,avg(LAST_1_MONTH_MID) as LAST_1_MONTH_MID
+,avg(LAST_1_MONTH_MID25) as LAST_1_MONTH_MID25
+,avg(LAST_1_MONTH_MID75) as LAST_1_MONTH_MID75
+,avg(LAST_2_MONTH_AVG) as LAST_2_MONTH_AVG
+,avg(LAST_2_MONTH_MIN) as LAST_2_MONTH_MIN
+,avg(LAST_2_MONTH_MAX) as LAST_2_MONTH_MAX
+,avg(LAST_2_MONTH_SUM) as LAST_2_MONTH_SUM
+,avg(LAST_2_MONTH_MID) as LAST_2_MONTH_MID
+,avg(LAST_2_MONTH_MID25) as LAST_2_MONTH_MID25
+,avg(LAST_2_MONTH_MID75) as LAST_2_MONTH_MID75
+,avg(LAST_3_MONTH_AVG) as LAST_3_MONTH_AVG
+,avg(LAST_3_MONTH_MIN) as LAST_3_MONTH_MIN
+,avg(LAST_3_MONTH_MAX) as LAST_3_MONTH_MAX
+,avg(LAST_3_MONTH_SUM) as LAST_3_MONTH_SUM
+,avg(LAST_3_MONTH_MID) as LAST_3_MONTH_MID
+,avg(LAST_3_MONTH_MID25) as LAST_3_MONTH_MID25
+,avg(LAST_3_MONTH_MID75) as LAST_3_MONTH_MID75
+,avg(LAST_4_MONTH_AVG) as LAST_4_MONTH_AVG
+,avg(LAST_4_MONTH_MIN) as LAST_4_MONTH_MIN
+,avg(LAST_4_MONTH_MAX) as LAST_4_MONTH_MAX
+,avg(LAST_4_MONTH_SUM) as LAST_4_MONTH_SUM
+,avg(LAST_4_MONTH_MID) as LAST_4_MONTH_MID
+,avg(LAST_4_MONTH_MID25) as LAST_4_MONTH_MID25
+,avg(LAST_4_MONTH_MID75) as LAST_4_MONTH_MID75
+,avg(LAST_5_MONTH_AVG) as LAST_5_MONTH_AVG
+,avg(LAST_5_MONTH_MIN) as LAST_5_MONTH_MIN
+,avg(LAST_5_MONTH_MAX) as LAST_5_MONTH_MAX
+,avg(LAST_5_MONTH_SUM) as LAST_5_MONTH_SUM
+,avg(LAST_5_MONTH_MID) as LAST_5_MONTH_MID
+,avg(LAST_5_MONTH_MID25) as LAST_5_MONTH_MID25
+,avg(LAST_5_MONTH_MID75) as LAST_5_MONTH_MID75
+,avg(LAST_6_MONTH_AVG) as LAST_6_MONTH_AVG
+,avg(LAST_6_MONTH_MIN) as LAST_6_MONTH_MIN
+,avg(LAST_6_MONTH_MAX) as LAST_6_MONTH_MAX
+,avg(LAST_6_MONTH_SUM) as LAST_6_MONTH_SUM
+,avg(LAST_6_MONTH_MID) as LAST_6_MONTH_MID
+,avg(LAST_6_MONTH_MID25) as LAST_6_MONTH_MID25
+,avg(LAST_6_MONTH_MID75) as LAST_6_MONTH_MID75
+,avg(LAST_12_MONTH_AVG) as LAST_12_MONTH_AVG
+,avg(LAST_12_MONTH_MIN) as LAST_12_MONTH_MIN
+,avg(LAST_12_MONTH_MAX) as LAST_12_MONTH_MAX
+,avg(LAST_12_MONTH_SUM) as LAST_12_MONTH_SUM
+,avg(LAST_12_MONTH_MID) as LAST_12_MONTH_MID
+,avg(LAST_12_MONTH_MID25) as LAST_12_MONTH_MID25
+,avg(LAST_12_MONTH_MID75) as LAST_12_MONTH_MID75
+ from df where BANNER_NAME='Carrefour' group by REPORT_DATE")
+
+banner_group_raw = read.csv("dataset\\banner_group.csv")
+
+banner_group=banner_group_raw[-2]
+head(banner_group)
+banner_trend = merge(trend, banner_group, by="REPORT_DATE")
+head(df_trend, 3)
+#y = df_trend[["QLI"]]
+#X = df_trend[-2]
+#splitSet = train_test_split(X, y, test_size=0.2)
+#X_train = splitSet$X_train
+#y_train = splitSet$y_train
+#X_test = splitSet$X_test
+#y_test = splitSet$y_test
+#head(X_train, 1)
+trainIndex = createDataPartition(df_trend$QLI,
+                                 p=0.8, list=FALSE,times=1)
+
+train = df_trend[trainIndex,]
+test = df_trend[-trainIndex,]
+rf = randomForest(QLI~., data=train,mtry=3,
+                  importance=TRUE, na.action=na.omit)
+df_trend["REPORT_DATE"]
