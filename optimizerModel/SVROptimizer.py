2af765d48c309199da20d7a591476f7586d04290
yad <yadhr582855555@hotmail.com>
2019-09-10 22:47:52 +0800

    split data

--- optimizerModel/SVROptimizer.py
+++ optimizerModel/SVROptimizer.py
@@ -7,13 +7,18 @@ class SVROptimizer(object):
         self.X = X_train
         self.y = y_train
 
-    def getOptimizedModel(self, X, y):
+    def get_optimized_model(self, X, y):
+        # simply print shape of dataset
         print("start SVR getOptimizedModel()")
+        for i in ["X", X, "y", y]:
+            if len(i) == 1:
+                print("train_%s shape:" % i, end="")
+            else:
+                print(str((np.shape(i))))
 
         # C :Penalty Coefficient ,1e3 is more suitable.
         # gamma :param of kernel ,1e-2 is more suitable.
-
-        # details:https://www.cnblogs.com/pinard/p/6117515.html 刘建平blog，第5点有SVR回归用法
+        # details: https://www.cnblogs.com/pinard/p/6117515.html 刘建平blog，第5点有SVR回归用法
         svr = SVR(kernel='rbf', C=1e3, gamma=0.01)
         svr.fit(X, y)
         return svr
--- test/ModelClassTestSVR.py
+++ test/ModelClassTestSVR.py
@@ -3,28 +3,29 @@ from matplotlib import pyplot as plt
 import os, sys
 import pandas as pd
 import warnings
+import numpy as np
 
 df_carre_month = pd.read_csv("..\\dataset\\banner_carr_month(1).csv")
 print(len(df_carre_month.values))
 
-train_y = df_carre_month["QLI"].values
+# select data without year of 2019 for train.
+train_dataset = df_carre_month[~df_carre_month['YEAR_OF_WEEK'].isin([2019])]
+# train_data of y
+train_y = train_dataset["QLI"].values
+# predict of X
 X = df_carre_month[df_carre_month['YEAR_OF_WEEK'].isin([2019])]
-train_X = df_carre_month[~df_carre_month['YEAR_OF_WEEK'].isin([2019])].drop(["Unnamed: 0","QLI"],axis=1)
+# train_data of X
+train_X = train_dataset.drop(["Unnamed: 0", "QLI"], axis=1)
 
+svr_opt = SVROptimizer(train_X, train_y)
+svr = svr_opt.get_optimized_model(train_X, train_y)
 
-svr_opt = SVROptimizer(train_X,train_y)
-svr = svr_opt.getOptimizedModel(train_X, train_y)
-
-predict = svr.predict(X)
-del df_carre_month["QLI"]
-
-plt.figure()
-plt.subplot(1, 2, 1)
-
-
-
-
-
+# predict = svr.predict(X)
+del df_carre_month, train_dataset
+#
+# plt.figure()
+# plt.subplot(1, 2, 1)
+#
 
 
 plt.subplot(1, 2, 2)
