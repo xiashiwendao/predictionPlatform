ab2b61edb29acbd374a322523d0552a104a65525
andi.you <yadhr582855555@126.com>
2019-09-11 17:30:37 +0800

    test for predict

--- test/ModelClassTestSVR.py
+++ test/ModelClassTestSVR.py
@@ -13,19 +13,19 @@ train_dataset = df_carre_month[~df_carre_month['YEAR_OF_WEEK'].isin([2019])]
 # train_data of y
 train_y = train_dataset["QLI"].values
 # predict of X
-X = df_carre_month[df_carre_month['YEAR_OF_WEEK'].isin([2019])]
+X = df_carre_month[df_carre_month['YEAR_OF_WEEK'].isin([2019])].drop(["Unnamed: 0", "QLI"], axis=1)
 # train_data of X
 train_X = train_dataset.drop(["Unnamed: 0", "QLI"], axis=1)
 
 svr_opt = SVROptimizer(train_X, train_y)
 svr = svr_opt.get_optimized_model(train_X, train_y)
 
-# predict = svr.predict(X)
+predict = svr.predict(X)
+print(predict)
 del df_carre_month, train_dataset
 #
 # plt.figure()
 # plt.subplot(1, 2, 1)
 #
-
 
 plt.subplot(1, 2, 2)
