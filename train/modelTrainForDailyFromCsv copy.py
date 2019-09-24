bd1f4a58a0b5ac2b099ab3b7d8a9c1c7c258998b
xiashiwenao <xiashiwendao@sina.com>
2019-09-24 21:44:26 +0800

    add daily train in home

---
+++ train/modelTrainForDailyFromCsv copy.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+#import pymysql
+import os, sys
+import datetime
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+import time
+import warnings
+
+warnings.filterwarnings('ignore')
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def real_predict_curve(model, X, y):
+    X_train = X[0:24]
+    y_train = y[0:24]
+    reg = model.fit(X_train, y_train)
+    yHat = reg.predict(X)
+    print("yHat value is: ")
+    for val in yHat:
+        print(val)
+    plt.plot(range(len(df_merge_clean)), labels, "b-")
+    plt.plot(range(len(df_merge_clean)), yHat, "r--")
+    
+    mse = mean_squared_error(yHat[-12:], y[-12:])
+    print("mse: ", mse, "rmse: ", np.sqrt(mse))
+    plt.show()
+
+basePath = "dataset"
+st = time.time()
+filePath = os.path.join(basePath, "banner_trends_carr_daily.csv")
+df_merge=pd.read_csv(filePath)
+labels = df_merge["QLI"].values
+df_merge_clean = df_merge.copy()
+df_merge_clean = df_merge_clean.drop("QLI", 1)
+
+df_merge_clean["YEARS"] =df_merge_clean["REPORT_DATE"].apply(lambda report_date:datetime.datetime.strptime(report_date,"%Y-%m-%d").timetuple().tm_year) 
+df_merge_clean["DAYS"] = df_merge_clean["REPORT_DATE"].apply(lambda report_date:datetime.datetime.strptime(report_date,"%Y-%m-%d").timetuple().tm_yday)
+df_merge_clean = df_merge_clean.drop("REPORT_DATE", axis=1)
+df_merge_clean.head()
+X = df_merge_clean.copy()
+y = labels.copy()
+X_train = X[0:655]
+y_train = y[0:655]
+X_test = X[655:]
+y_test = y[655:]
+lr = LinearRegression()
+real_predict_curve(lr, X, y)
+
+rf = RandomForestRegressor()
+# real_predict_curve(rf, df_merge_clean, labels)
+# X_train = X[0:24]
+# y_train = y[0:24]
+reg = rf.fit(X_train, y_train)
+feature_import = rf.feature_importances_
+y_test_Hat = reg.predict(X_test)
+print("yHat value is: ")
+for val in y_test_Hat:
+    print(val)
+mse = mean_squared_error(y_test_Hat, y_test)
+print("mse: ", mse, "rmse: ", np.sqrt(mse))
+plt.plot(range(len(df_merge_clean)), yHat, "r-")
+plt.plot(range(len(df_merge_clean)), labels, "b--")
+plt.show()
+
+# svr =SVR()
+# real_predict_curve(svr, df_merge_clean, labels)
+
+gbr = GradientBoostingRegressor()
+# real_predict_curve(gbr, df_merge_clean, labels)
+reg = gbr.fit(X_train, y_train)
+yHat = reg.predict(X)
+print("yHat value is: ")
+for val in yHat:
+    print(val)
+mse = mean_squared_error(yHat[-12:], y[-12:])
+print("mse: ", mse, "rmse: ", np.sqrt(mse))
+plt.plot(range(len(df_merge_clean)), yHat, "r-")
+plt.plot(range(len(df_merge_clean)), labels, "b--")
+plt.show()
+
+def toOne(ar):
+    min_val = np.min(ar)
+    max_val = np.max(ar)
+    one_value = (ar - min_val)/(max_val - min_val)
+
+    return one_value
+
+print("OK")
+toOne(feature_import)
+for corr in df_merge.corr()["QLI"]:
+    print(corr)
+
